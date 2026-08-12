912f1a44d60bf16dd1b824bb8a6e70be0d3ede6d
dipakbhatt-09 <bhattdipak90@gmail.com>
2026-08-12 18:45:41 +0545

    Fix production media serving

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,10 +7,15 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
 
+    # Portfolio API
     path("api/", include("portfolio.urls")),
+
+    # Contact API
     path("api/", include("contact.urls")),
 ]
 
+
+# Serve media files
 urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
