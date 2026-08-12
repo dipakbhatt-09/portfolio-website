7e7f9e6efd9dcc7e328c30bcaac623084b95fd6e
dipakbhatt-09 <bhattdipak90@gmail.com>
2026-08-12 17:41:22 +0545

    Update gitignore and API configuration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://portfolio-website-fus9.onrender.com/api/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
