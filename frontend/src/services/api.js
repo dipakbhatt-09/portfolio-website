e90b47aef15c79cf421d6be0396c92f3f828d961
dipakbhatt-09 <bhattdipak90@gmail.com>
2026-08-12 15:30:27 +0545

    Connect portfolio frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://portfolio-website-yx51.onrender.com/api/",
+    baseURL: "https://portfolio-website-fus9.onrender.com/api/",
 });
 
 export default api;
